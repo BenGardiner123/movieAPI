9ecb4a0290a64ac3e0acb1f88bc66bf842a2d113
Ben Gardiner <bennygards@gmail.com>
2021-05-12 13:42:48 +1000

    setting up the in memory stuff for inital testing

---
+++ movieAPI/Services/inMemoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using movieAPI.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movieAPI.Services
+{
+    public class inMemoryRepository
+    {
+        private List <Genre>
+
+        public inMemoryRepository()
+        {
+
+        }
+    }
+}
