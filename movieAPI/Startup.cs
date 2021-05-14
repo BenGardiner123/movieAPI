e0102611b82edb24b6158d46c9a8ac6e9a559ed5
Ben Gardiner <bennygards@gmail.com>
2021-05-14 22:22:44 +1000

    add som ecomments in the http request pipeline

--- movieAPI/Startup.cs
+++ movieAPI/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -40,8 +41,16 @@ namespace movieAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //haha the comment is right there but i only just made sense that this is what a pipeline is
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //if i put this here.. no matter what endpoint i hit the message will always display.
+            //its called "short circuiting the pipeline"
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("im stopping this here");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
