04740977e8c7f73acb3743c0ce379c03bd74ff25
Ben Gardiner <bennygards@gmail.com>
2021-05-18 13:33:50 +1000

    blob logic in the controller

--- movieAPI/Helpers/AzureStorageService.cs
+++ movieAPI/Helpers/AzureStorageService.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +11,11 @@ namespace movieAPI.Helpers
 {
     public class AzureStorageService : IFileStorageService
     {
-        public AzureStorageService()
-        {
+        private string connectionString;
 
+        public AzureStorageService(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("AzureStorageConnection");
         }
      
         public Task DeleteFile(string fileRoute, string containerName)
@@ -23,9 +28,25 @@ namespace movieAPI.Helpers
             throw new NotImplementedException();
         }
 
-        public Task<string> SaveFile(string containerName, IFormFile file)
+        public async Task<string> SaveFile(string containerName, IFormFile file)
         {
-            throw new NotImplementedException();
+            // create the blob connection instance
+            var client = new BlobContainerClient(connectionString, containerName);
+            //check if the blob exists and creaete otherwise conenct
+            await client.CreateIfNotExistsAsync();
+            //set access policy
+            client.SetAccessPolicy(Azure.Storage.Blobs.Models.PublicAccessType.Blob);
+            //create a random filename
+            var extenstion = Path.GetExtension(file.FileName);
+
+            var fileName = $"{Guid.NewGuid()}{extenstion}";
+
+            //now add the new filename to the connection client
+            var blob = client.GetBlobClient(fileName);
+            //upload the files async styles and then once completed give us the URL that we can use in the front end
+            await blob.UploadAsync(file.OpenReadStream());
+            return blob.Uri.ToString();
+
         }
     }
 }
