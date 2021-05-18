a00a54655cf31e8f08a0402ddfb8004a856fa53e
Ben Gardiner <bennygards@gmail.com>
2021-05-18 13:22:46 +1000

    adding the interfaces, adding azure conn, impleenting interface

---
+++ movieAPI/Helpers/AzureStorageService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movieAPI.Helpers
+{
+    public class AzureStorageService : IFileStorageService
+    {
+        public AzureStorageService()
+        {
+
+        }
+     
+        public Task DeleteFile(string fileRoute, string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> EditFile(string containerName, IFormFile file, string fileRoute)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> SaveFile(string containerName, IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ movieAPI/Helpers/IFileStorageService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movieAPI.Helpers
+{
+    interface IFileStorageService
+    {
+        Task DeleteFile(string fileRoute, string containerName);
+        Task<String> SaveFile(string containerName, IFormFile file);
+        Task<String> EditFile(string containerName, IFormFile file, string fileRoute);
+
+    }
+}
