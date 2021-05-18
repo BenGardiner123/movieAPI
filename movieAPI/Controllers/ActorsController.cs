7eec1484b4ca60d18762b3741f3128015a269877
Ben Gardiner <bennygards@gmail.com>
2021-05-18 11:50:38 +1000

    adding the actor controller and 2 endpoints

---
+++ movieAPI/Controllers/ActorsController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using movieAPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movieAPI.Controllers
+{
+    [Route("api/actors")]
+    [ApiController]
+    public class ActorsController: ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public ActorsController(ApplicationDbContext dbContext, IMapper mapper)
+        {
+            this._dbContext = dbContext;
+            this._mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ActorDTO>>> Get()
+        {
+            var actors = await _dbContext.Actors.ToListAsync();
+            return _mapper.Map<List<ActorDTO>>(actors);
+
+
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ActorDTO>> Get(int id)
+        {
+            var actor = await _dbContext.Actors.FirstOrDefaultAsync( x => x.Id == id);
+
+            if(actor == null)
+            {
+                return NotFound();
+            }
+
+            return _mapper.Map<ActorDTO>(actor);
+
+
+        }
+
+
+    }
+}
