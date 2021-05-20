f1fcfa691c25d4a288d02e6c7d0f29715d7b2939
Ben Gardiner <bennygards@gmail.com>
2021-05-20 21:40:26 +1000

    adding a movie controller and custom type binder

---
+++ movieAPI/Controllers/MovieController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using movieAPI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movieAPI.Controllers
+{
+    public class MovieController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+        private readonly IMapper mapper;
+        private readonly IFileStorageService storageService;
+
+        public MovieController(ApplicationDbContext dbContext, IMapper mapper, IFileStorageService storageService)
+        {
+            this.dbContext = dbContext;
+            this.mapper = mapper;
+            this.storageService = storageService;
+        }
+
+        [HttpPost]
+        private async Task<ActionResult> Post([FromForm])
+        {
+
+        }
+
+
+    }
+
+
+}
---
+++ movieAPI/DTOs/MovieCreationDTO.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movieAPI.DTOs
+{
+    public class MovieCreationDTO
+    {
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Trailer { get; set; }
+        public bool InTheaters { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public IFormFile Poster { get; set; }
+        
+    }
+}
